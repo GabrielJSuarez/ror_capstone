441ecfbba1b9daad084bde080b84f16f799ace75
Gabriel Ginn Suárez <ginnlaiho@gmail.com>
2021-05-11 09:56:28 -0500

    - Views: Add secondary navigation to web app - Feature: Add controller level creation tool for projects with routes

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,14 +1,31 @@
 class ProjectController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @projects = Project.all.page params[:page]
   end
 
   def new
+    @project = current_user.projects.build
   end
 
   def create
+    @project = current_user.projects.build(projects_param)
+    if @project.save
+      flash[:success] = 'Project created successfully!'
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def projects_params
+    params.require(:project).permit(:name, :time)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :project, only: %i[index new create show]
   root 'project#index'
+
+  get '/welcome', to: 'welcome#index'
 end
