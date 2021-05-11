ef252f4f8f1cc6c03222fabd2122bddfe3d0d1b4
Gabriel Ginn Suárez <ginnlaiho@gmail.com>
2021-05-11 14:55:21 -0500

    - Fix: Fix a bug that shows in the footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :project, only: %i[index new create show]
-  root 'project#index'
-
-  get '/home', to: 'home#home'
+  root 'home#home'
 end
