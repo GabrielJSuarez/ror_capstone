ebbbe0339318a96151c2d71c48f554c20c156137
Gabriel Ginn Suárez <ginnlaiho@gmail.com>
2021-05-12 09:47:55 -0500

    - Feature: Create groups scaffold code

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,31 @@
+class GroupsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @groups = Group.all
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = current_user.groups.build
+  end
+
+  def create
+    @group = current_user.groups.build(groups_param)
+    if @group.save
+      flash[:success] = 'Group created!'
+      redirect_to groups_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def groups_param
+    params.require(:group).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :project, only: %i[index new create show]
-  resources :group, only: %i[index new create show]
+  resources :groups, only: %i[index new create show]
   get 'projects/external', to: 'project#external'
   root 'home#home'
 end
