e0790c2e9f73ec77e4e7830819ebdc06bc9f4602
Gabriel Ginn Suárez <ginnlaiho@gmail.com>
2021-05-14 14:28:53 -0500

    Bugfixes in the views

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   has_many :logs
   has_many :groups, through: :logs
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :time, presence: true, numericality: { only_integer: true }
 end
