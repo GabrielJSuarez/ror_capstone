669f2978c2f6554703bf73f5549d854034937381
Gabriel Ginn Suárez <ginnlaiho@gmail.com>
2021-05-14 20:09:45 -0500

    fix a bug in the add project to group page

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -1,2 +1,9 @@
 module LogsHelper
+  def check_projects
+    if @project_options.any?
+      render 'shared/log', object: @log
+    else
+      link_to 'Nothing to add, go back', authenticated_root_path, class: 'nav-link text-center'
+    end
+  end
 end
